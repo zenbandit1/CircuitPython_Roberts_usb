9021edc2eb952c7d8ecc94928db97b8f06352e5e
zenbandit1 <suffolkbookkeeping@gmail.com>
2022-07-18 19:59:58 +0100

    modified:   roberts_usb.py

--- roberts_usb.py
+++ roberts_usb.py
@@ -16,16 +16,16 @@ Implementation Notes
 
 **Hardware:**
 
-.. todo:: Add links to any specific hardware product page(s), or category page(s).
-  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
+#.. todo:: Add links to any specific hardware product page(s), or category page(s).
+#  Use unordered list & hyperlink rST inline format: "* `Link Text <url>`_"
 
-**Software and Dependencies:**
+#**Software and Dependencies:**
 
-* Adafruit CircuitPython firmware for the supported boards:
-  https://circuitpython.org/downloads
+#* Adafruit CircuitPython firmware for the supported boards:
+#  https://circuitpython.org/downloads
 
-.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
-  based on the library's use of either.
+#.. todo:: Uncomment or remove the Bus Device and/or the Register library dependencies
+#  based on the library's use of either.
 
 # * Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
 # * Adafruit's Register library: https://github.com/adafruit/Adafruit_CircuitPython_Register
